97e4c1882ddb64387104a7b182f60facab95dd30
Ehsan Foadian <ehsanfoadian@gmail.com>
2023-11-05 09:09:52 +0330

    [feat] All rules were connected and called by PuzzleInputHandler.cs by using reflections    - Box collider 2d was attached to Tile prefab to get OnMouseDown message from Unity

--- Assets/Scripts/PuzzleScene/PuzzleInputHandler.cs
+++ Assets/Scripts/PuzzleScene/PuzzleInputHandler.cs
@@ -1,19 +1,39 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 public class PuzzleInputHandler : MonoBehaviour
 {
-    private Puzzle _puzzel;
+    private Puzzle _puzzle;
     IRuleTileTap[] tapRules = null;
 
     private void OnEnable()
     {
-        _puzzel= GetComponent<Puzzle>();
+        _puzzle = GetComponent<Puzzle>();
 
+        //finds all types which implmented IRuleTileTap
+        Type[] types = null;
+        {
+            types = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => typeof(IRuleTileTap).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+            .ToArray();
+        }
 
+        //creates instance for all of found rule classes
+        tapRules = new IRuleTileTap[types.Length];
+        for(int i=0; i<types.Length; ++i)
+        {
+            IRuleTileTap r = (IRuleTileTap)Activator.CreateInstance(types[i]);
+            tapRules[i] = r;
+        }
     }
 
     public void OnTapHandler(Vector2Int pos)
     {
-
+        for(int i=0; i<tapRules.Length; ++i) 
+        {
+            tapRules[i].Execute(pos, _puzzle);
+        }
     }
 }
