4fbb166b42bb7fbe4aa6fe6186e77435e5d32f7e
Ehsan Foadian <ehsanfoadian@gmail.com>
2023-11-09 22:32:57 +0330

    [improve] Naming was improved

---
+++ Assets/Scripts/PuzzleScene/PuzzleLogic/FillEmptyCellsUtil.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class PuzzleLogic
+{
+    public static void FillEmptyCellsUtil(in int[,] typeGrid, in Vector2Int tapPos)
+    {
+        
+    }
+}
