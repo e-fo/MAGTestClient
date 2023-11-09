43b3d0832e63e5ff383af8d2b4cb689f854d8736
Ehsan Foadian <ehsanfoadian@gmail.com>
2023-11-09 12:49:15 +0330

    [feat] Find island indices of selected tile in grid was implemented.

--- Assets/Scripts/PuzzleScene/PopIslandRule.cs
+++ Assets/Scripts/PuzzleScene/PopIslandRule.cs
@@ -8,5 +8,14 @@ public struct PopIslandRule: IRuleTileTap
         var go = puzzle.TilesRefComponents[tile.GameObjectInstanceId];
 
         Debug.Log($"You tapped on tile with position: {go.Transform.position}");
+        var typeGrid = PuzzleUtil.GetTypeGrid(puzzle.Table);
+        var islandIndicies = PuzzleUtil.GetIslandIndices(typeGrid, position);
+
+        foreach(Vector2Int idx in islandIndicies)
+        {
+            Debug.Log($"x:{idx.x}-y:{idx.y}");
+            puzzle.TilesRefComponents[puzzle.Table[idx.x, idx.y].GameObjectInstanceId]
+                .Transform.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 }
--- Assets/Scripts/PuzzleScene/PuzzleUtil.cs
+++ Assets/Scripts/PuzzleScene/PuzzleUtil.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -18,5 +19,53 @@ public static class PuzzleUtil
         return (
             new TileStateValue(tile.GetInstanceID(), configInstanceId), 
             new TileStateRef(tile.transform));
+    }
+
+    public static int[,] GetTypeGrid(in TileStateValue[,] grid)
+    {
+        int rows = grid.GetLength(0);
+        int cols = grid.GetLength(1);
+
+        int[,] ret = new int[rows, cols];
+        
+        for(int i=0; i<rows; ++i) {
+            for(int j=0; j<cols; ++j) {
+                ret[i,j] = grid[i,j].SOEnumTypeInstanceId;
+            }
+        }
+        return ret;
+    }
+
+    public static Vector2Int[] GetIslandIndices(in int[,] typeGrid, in Vector2Int tapPos)
+    {
+        List<Vector2Int> ret = new();
+
+        int rows = typeGrid.GetLength(0);
+        int cols = typeGrid.GetLength(1);
+
+        bool[,] visited = new bool[rows, cols];
+        int selected = typeGrid[tapPos.x, tapPos.y];
+
+        Stack<Vector2Int> stack = new();
+        stack.Push(tapPos);
+
+        while(stack.Count > 0) {
+            Vector2Int current = stack.Pop();
+            int i = current.x;
+            int j = current.y;
+
+            if(i < 0 || j < 0 || i >= rows || j >= cols || visited[i, j] || typeGrid[i, j] != selected) {
+                continue;
+            }
+
+            visited[i, j] = true;
+            ret.Add(new Vector2Int(i,j));
+            stack.Push(new Vector2Int(i + 1, j)); 
+            stack.Push(new Vector2Int(i - 1, j)); 
+            stack.Push(new Vector2Int(i, j + 1));
+            stack.Push(new Vector2Int(i, j - 1)); 
+        }
+
+        return ret.ToArray();
     }
 }
