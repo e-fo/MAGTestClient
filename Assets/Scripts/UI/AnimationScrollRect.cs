2165684f9d31bcf7422e3a1c71ff3e6b180628d9
Ehsan Foadian <ehsanfoadian@gmail.com>
2025-04-11 09:08:36 +0330

    [feature][improve] Implementing carousel UI and reorganizing assemblies.

---
+++ Assets/Scripts/UI/AnimationScrollRect.cs
@@ -0,0 +1,194 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using DG.Tweening;
+
+public interface ICellData { };
+
+public class AnimationScrollRect : MonoBehaviour
+{
+    [SerializeField, Range(1e-2f, 1f)] private float _cellInterval = 0.2f;
+    [SerializeField, Range(0f, 1f)] private float _scrollOffset = 0.5f;
+    [SerializeField] private bool _loop = false;
+    [SerializeField] private Transform _cellContainer = default;
+    [SerializeField] MyScroller _scroller = default;
+    public MyScroller Scroller => _scroller;
+
+    private bool _initialized;
+    private float _currentPosition;
+    private int _selectedIndex;
+    public int SelectedIndex => _selectedIndex;
+
+    [SerializeField] private Cell _cellPrefab;
+    private readonly List<Cell> _pool = new();
+    private List<ICellData> _contentList { get; set; } = new();
+
+    public void UpdateData(List<ICellData> contents)
+    {
+        _contentList.Clear();
+        _contentList.AddRange(contents);
+
+        UpdatePosition(_currentPosition, true);
+        _scroller.TotalCount = contents.Count;
+    }
+
+    public void SelectCell(int index)
+    {
+        if (index < 0 || index >= _contentList.Count || index == _selectedIndex)
+        {
+            if (_loop)
+            {
+                index = ArrayUtil.CircularIndex(index, _contentList.Count);
+            } else
+            {
+                return;
+            }
+        }
+
+        UpdateSelection(index);
+        _scroller.ScrollTo(index, 0.35f, Ease.OutCubic);
+    }
+
+    private void UpdatePosition(float position, bool forceRefresh)
+    {
+        if (!_initialized)
+        {
+            _scroller.OnValueChanged += scrollerValue => UpdatePosition(scrollerValue, false);
+            _scroller.OnSelectionChanged += UpdateSelection;
+            _initialized = true;
+        }
+
+        _currentPosition = position;
+
+        var p = position - _scrollOffset / _cellInterval;
+        var firstIndex = Mathf.CeilToInt(p);
+        var firstPosition = (Mathf.Ceil(p) - p) * _cellInterval;
+
+        ResizingPool(firstPosition);
+
+        UpdateCells(firstPosition, firstIndex, forceRefresh);
+
+        UpdateOrdering(_pool);
+    }
+
+    private void ResizingPool(float firstPosition)
+    {
+        if (firstPosition + _pool.Count * _cellInterval < 1f)
+        {
+            Debug.Assert(_cellPrefab != null);
+            Debug.Assert(_cellContainer != null);
+
+            var addCount = Mathf.CeilToInt((1f - firstPosition) / _cellInterval) - _pool.Count;
+            for (var i = 0; i < addCount; i++)
+            {
+                var cell = Instantiate(_cellPrefab, _cellContainer).GetComponent<Cell>();
+                if (cell == null)
+                {
+                    throw new MissingComponentException(string.Format(
+                "FancyCell<{0}, {1}> component not found in {2}.",
+                typeof(Cell).FullName, typeof(Cell).FullName, _cellPrefab.name));
+                }
+
+                cell.Initialize(this);
+
+                _pool.Add(cell);
+            }
+        }
+    }
+
+    private void UpdateCells(float firstPosition, int firstIndex, bool forceRefresh)
+    {
+        for (var i = 0; i < _pool.Count; i++)
+        {
+            var index = firstIndex + i;
+            var pos = firstPosition + i * _cellInterval;
+            var cell = _pool[ArrayUtil.CircularIndex(index, _pool.Count)];
+
+            if (_loop)
+            {
+                index = ArrayUtil.CircularIndex(index, _contentList.Count);
+            }
+
+            if (index < 0 || index >= _contentList.Count || pos > 1f)
+            {
+                cell.Disable();
+                continue;
+            }
+
+            if (forceRefresh || cell.Index != index || !cell.gameObject.activeSelf)
+            {
+                cell.Index = index;
+                cell.Enable(_contentList[index]);
+            }
+
+            cell.UpdatePosition(pos);
+        }
+    }
+
+    private void UpdateOrdering(IList<Cell> cells)
+    {
+        List<Cell> activeCells = cells.Where(c => c.gameObject.activeInHierarchy).OrderBy(c => c.CurrentPosition).ToList();
+
+        int middleIndex = 0;
+        for(int i=1; i<activeCells.Count; ++i)
+        {
+            if (Mathf.Abs(activeCells[i].CurrentPosition - 0.5f) < Mathf.Abs(activeCells[middleIndex].CurrentPosition - 0.5f))
+            {
+                middleIndex = i;
+            }
+        }
+
+        float selectedCellPosition = activeCells[middleIndex].CurrentPosition;
+        int c = activeCells.Count;
+        for (var i = 0; i < activeCells.Count; ++i)
+        {
+            if (activeCells[i].CurrentPosition < selectedCellPosition)
+            {
+                activeCells[i].transform.SetSiblingIndex(i);
+            } else
+            {
+                c--;
+                if (activeCells[c].CurrentPosition > selectedCellPosition)
+                {
+                    activeCells[c].transform.SetAsLastSibling();
+                }
+            }
+        }
+
+        activeCells[middleIndex].transform.SetAsLastSibling();
+    }
+
+    private void UpdateSelection(int index)
+    {
+        if (_selectedIndex == index)
+        {
+            return;
+        }
+
+        _selectedIndex = index;
+        UpdatePosition(_currentPosition, true);
+
+        _pool.FirstOrDefault(c => c.Index == _selectedIndex)?.transform.SetAsLastSibling();
+
+        //onSelectionChanged?.Invoke(index);
+    }
+
+#if UNITY_EDITOR
+    bool _EDITOR_CachedLoop;
+    float _EDITOR_CachedCellInterval, _EDITOR_CachedScrollOffset;
+
+    void LateUpdate()
+    {
+        if (_EDITOR_CachedLoop != _loop ||
+            _EDITOR_CachedCellInterval != _cellInterval ||
+            _EDITOR_CachedScrollOffset != _scrollOffset)
+        {
+            _EDITOR_CachedLoop = _loop;
+            _EDITOR_CachedCellInterval = _cellInterval;
+            _EDITOR_CachedScrollOffset = _scrollOffset;
+
+            UpdatePosition(_currentPosition, false);
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/UI/MyScroller.cs
@@ -0,0 +1,283 @@
+using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using static UnityEngine.UI.ScrollRect;
+
+public enum ScrollDirection
+{
+    Vertical,
+    Horizontal,
+}
+
+public enum MovementDirection
+{
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+public class MyScroller : UIBehaviour, IPointerUpHandler, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [Serializable]
+    private class Snap
+    {
+        public bool Enable;
+        public float Duration;
+        public float VelocityThreshold;
+        public Ease Easing;
+    }
+
+    public event Action<float> OnValueChanged;
+    public event Action<int> OnSelectionChanged;
+    public ScrollDirection ScrollDirection = ScrollDirection.Vertical;
+    public MovementType MovementType = MovementType.Elastic;
+    public float Elasticity = 0.1f;
+    public float ScrollSensitivity = 1f;
+    public bool Inertia = true;
+    public float DecelerationRate = 0.03f;
+    public bool Draggable = true;
+    public int TotalCount;
+    private Vector2 _beginDragPosition;
+    private float _startPosition;
+    private float _velocity;
+    private bool _dragging;
+    private Tween _autoScrollTween;
+
+    [SerializeField] private RectTransform _viewport = default;
+    public float ViewportSize => ScrollDirection == ScrollDirection.Horizontal
+    ? _viewport.rect.size.x
+    : _viewport.rect.size.y;
+
+    [SerializeField]
+    Snap _snap = new Snap
+    {
+        Enable = true,
+        VelocityThreshold = 0.5f,
+        Duration = 0.3f,
+        Easing = Ease.InOutCubic
+    };
+    public bool SnapEnabled
+    {
+        get => _snap.Enable;
+        set => _snap.Enable = value;
+    }
+
+    public float CurrentPosition { get; private set; }
+
+    public void ScrollTo(float endPos, float duration, Ease ease)
+    {
+        if (duration <= 0f)
+        {
+            CurrentPosition = ArrayUtil.CircularPosition(endPos, TotalCount);
+            ResetAutoScrolling();
+            return;
+        }
+
+        if(_autoScrollTween.IsActive()) _autoScrollTween.Kill();
+
+        _autoScrollTween = DOTween.To(()=>CurrentPosition, f=>{
+            CurrentPosition = f;
+            OnValueChanged?.Invoke(CurrentPosition);
+        },
+        endPos, 
+        duration
+        ).SetEase(ease).OnComplete(()=>{
+            if(_snap.Enable)
+            {
+                CurrentPosition = Mathf.Clamp(Mathf.RoundToInt(CurrentPosition), 0, TotalCount - 1);
+                OnValueChanged?.Invoke(CurrentPosition);
+            }
+        });
+        
+        _velocity = 0;
+        _startPosition = CurrentPosition;
+        OnSelectionChanged?.Invoke(Mathf.RoundToInt(ArrayUtil.CircularPosition(endPos, TotalCount)));
+    }
+
+    public void JumpTo(int index)
+    {
+        if (index < 0 || index > TotalCount - 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+
+        CurrentPosition = index;
+        ResetAutoScrolling();
+        OnSelectionChanged?.Invoke(index);
+    }
+
+    private void ResetAutoScrolling()
+    {
+        if(_autoScrollTween.IsActive())
+        {
+            _autoScrollTween.Kill();
+        }
+        _velocity = 0;
+    }
+
+    private float GetOffset(float pos)
+    {
+        float ret = 0;
+
+        if (MovementType == MovementType.Unrestricted)
+        {
+            ret = 0f;
+        }
+        else if (pos < 0f)
+        {
+            ret = -pos;
+        }
+        else if (pos > TotalCount - 1)
+        {
+            ret = TotalCount - 1 - pos;
+        }
+        return ret;
+    }
+
+    public float Magnet(float overScrolling, float viewSize) =>
+        (1 - 1 / (Mathf.Abs(overScrolling) * 0.55f / viewSize + 1)) * viewSize * Mathf.Sign(overScrolling);
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        if (Draggable)
+        {
+            ResetAutoScrolling();
+        }
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        if (Draggable && _snap.Enable)
+        {
+            //ScrollTo(Mathf.RoundToInt(CurrentPosition), _snap.Duration, _snap.Easing);
+        }
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        if (Draggable)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _viewport,
+                eventData.position,
+                eventData.pressEventCamera,
+                out _beginDragPosition
+                );
+
+            _startPosition = CurrentPosition;
+            _dragging = true;
+            ResetAutoScrolling();
+        }
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        bool clickInside = RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _viewport,
+            eventData.position,
+            eventData.pressEventCamera,
+            out var dragPosition);
+
+        if (Draggable && clickInside)
+        {
+            Vector2 delta = dragPosition - _beginDragPosition;
+            float pos = (ScrollDirection == ScrollDirection.Horizontal ? -delta.x : delta.y)
+               / ViewportSize
+               * ScrollSensitivity
+               + _startPosition;
+
+            float offset = GetOffset(pos);
+            pos += GetOffset(pos);
+
+
+            if(MovementType == MovementType.Elastic)
+            {
+                pos -= Magnet(offset, ScrollSensitivity);
+            }
+
+            CurrentPosition = pos;
+            OnValueChanged?.Invoke(CurrentPosition);
+        }
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        if(Draggable)
+        {
+            _dragging = false;
+        }
+    }
+
+    void Update()
+    {
+        var deltaTime = Time.unscaledDeltaTime;
+        var offset = GetOffset(CurrentPosition);
+
+        if( !_autoScrollTween.IsActive() )
+        {
+            float pos = CurrentPosition;
+            if(!_dragging && (!Mathf.Approximately(_velocity, 0) || !Mathf.Approximately(offset, 0)))
+            {
+                if (Inertia)
+                {
+                    if (MovementType == MovementType.Elastic && !Mathf.Approximately(offset, 0f))
+                    {
+                        pos = Mathf.SmoothDamp(CurrentPosition, CurrentPosition + offset, ref _velocity,
+                            Elasticity, Mathf.Infinity, deltaTime);
+
+                        if (Mathf.Abs(_velocity) < 0.01f)
+                        {
+                            pos = Mathf.Clamp(Mathf.RoundToInt(pos), 0, TotalCount - 1);
+                            _velocity = 0f;
+                        }
+
+                    } else
+                    {
+                        _velocity *= Mathf.Pow(DecelerationRate, deltaTime);
+                        pos += _velocity * deltaTime;
+                        if (MovementType == MovementType.Elastic)
+                        {
+                            if (offset != 0f)
+                            {
+                                pos -= Magnet(offset, ScrollSensitivity);
+                            }
+                        }
+
+                        if (_snap.Enable && Mathf.Abs(_velocity) < _snap.VelocityThreshold)
+                        {
+                            ScrollTo(Mathf.RoundToInt(CurrentPosition), _snap.Duration, _snap.Easing);
+                        }
+                    }
+                } 
+                else
+                {
+                    _velocity = 0;
+                }
+
+                if (MovementType == MovementType.Clamped)
+                {
+                    offset = GetOffset(pos);
+                    pos += offset;
+                    if (Mathf.Approximately(pos, 0f) || Mathf.Approximately(pos, TotalCount - 1f))
+                    {
+                        _velocity = 0f;
+                        OnSelectionChanged?.Invoke(Mathf.RoundToInt(pos));
+                    }
+                }
+                CurrentPosition = pos;
+                OnValueChanged?.Invoke(CurrentPosition);
+            }
+
+            if (Inertia && _dragging)
+            {
+                var newVelocity = (CurrentPosition - _prevPosition) / deltaTime;
+                _velocity = Mathf.Lerp(_velocity, newVelocity, deltaTime * 10f);
+            }
+            _prevPosition = CurrentPosition;
+        }
+    }
+    float _prevPosition;
+}
